a98a60efb954443e81db624dceb988ab37e9541a
BlueDragon541 <118122865+BlueDragon541@users.noreply.github.com>
2023-04-02 12:11:23 -0500

    draw dots on face

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function playMyVideo(){
     console.log("Now playing webcam...");
 
     // Start Facetracker
-    //startFT();
+    startFT();
 
     // Draw my face
     drawMyFace();
@@ -87,7 +87,7 @@ function drawMyFaceDots(positions){
   for(let i=0;i<positions.length;i++){
     const posX = positions[i][0];
     const posY = positions[i][1];
-    // draw the dot with drawCircle
+    drawCircle(posX,posY, 'copper');
     if(debugMode){
         // write the dot label on the screen with drawText
     }
